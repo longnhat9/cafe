0904805d2bdb29228b85a27be574908c414577fc
longnhat9 <longnhatstar2002@gmail.com>
2024-01-22 15:30:04 +0700

    update

--- src/main/java/vn/viettuts/cafe/App.java
+++ src/main/java/vn/viettuts/cafe/App.java
@@ -3,19 +3,16 @@ package vn.viettuts.cafe;
 import java.awt.EventQueue;
 
 import vn.viettuts.cafe.controller.LoginController;
-import vn.viettuts.cafe.view.CafeManagerView;
 import vn.viettuts.cafe.view.LoginView;
 
 public class App {
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
-                CafeManagerView cafeManagerView = new CafeManagerView();
-                cafeManagerView.setVisible(true);
-//                LoginView view = new LoginView();
-//                LoginController controller = new LoginController(view);
-                // hiển thị màn hình login
-//                controller.showLoginView();
+                LoginView view = new LoginView();
+                LoginController controller = new LoginController(view);
+                //hiển thị màn hình login
+                controller.showLoginView();
             }
         });
     }
